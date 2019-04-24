ced9178fcfb5adb18c841c3cfff00ee47b76d910
root <root@ip-10-1-1-30.ec2.internal>
2019-04-24 08:37:34 +0000

    Correcting code

--- self-heal/setup.sh
+++ self-heal/setup.sh
@@ -7,14 +7,9 @@
 cd rabbitmq
 docker network create isolated_nw
 docker-compose up -d
-rabbit_container_id=$(docker ps -aqf "name=rabbitmq_q")
-echo $rabbit_container_id
-rabbitmq_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $rabbit_container_id)
-echo $rabbitmq_ip
 
 #2. Updating RabbitMQ in docker-entrypoint.sh and building new alerta image
 cd ../alerta
-sed -i "s/rabbitmq_ip/$rabbitmq_ip/gi" docker-entrypoint.sh
 #VCS_REF=$(git rev-parse --short HEAD)
 BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 VERSION=7.0.0
